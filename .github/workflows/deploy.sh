195229498bb3b477455a4c5feb272d21861767f8
Zhian N. Kamvar <zkamvar@gmail.com>
2024-08-21 14:49:33 -0700

    use url

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 set -x
 echo "AID: ${artifact_id:-missing}"
+echo "Artifact URL: ${artifact_url:-missing}"
 echo "Deploy ID: ${deploy_id:-missing}"
 curl -L \
   -X POST \
@@ -8,5 +9,5 @@ curl -L \
   -H "Authorization: Bearer ${GH_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${repo}/pages/deployments \
-  -d '{"artifact_id":'"${artifact_id}"',"environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${ID_TOKEN}"'","preview":false}'
+  -d '{"artifact_id":"'"${artifact_url}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${ID_TOKEN}"'","preview":false}'
 
