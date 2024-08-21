7c9220722d6ad1aca83b106d188c172dbad4cc72
Zhian N. Kamvar <zkamvar@gmail.com>
2024-08-21 14:24:36 -0700

    attempt to get things to werk

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,8 +4,8 @@ echo "Deploy ID: ${deploy_id:-missing}"
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_TOKEN}" \
+  -H "Authorization: Bearer <YOUR-TOKEN>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${repo}/pages/deployments \
-  -d "{\"artifact_id\":\"${artifact_id}\",\"environment\":\"github-pages\",\"pages_build_version\":\"${deploy_id}\",\"oidc_token\":\"${GH_TOKEN}\",\"preview\":false}"
+  -d '{"artifact_id":"'"${artifact_id}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${GH_TOKEN}"'","preview":false}'
 
