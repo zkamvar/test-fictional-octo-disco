33d37f94096c005cfadd377cf22d9571e374a788
Zhian N. Kamvar <zkamvar@gmail.com>
2024-08-21 14:33:24 -0700

    try the request token

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,5 +7,5 @@ curl -L \
   -H "Authorization: Bearer <YOUR-TOKEN>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${repo}/pages/deployments \
-  -d '{"artifact_id":"'"${artifact_id}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${GH_TOKEN}"'","preview":false}'
+  -d '{"artifact_id":"'"${artifact_id}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${ACTIONS_ID_TOKEN_REQUEST_TOKEN}"'","preview":false}'
 
