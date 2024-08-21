c0cc248eef9fee1d949403a9f89651e70fb7a03b
Zhian N. Kamvar <zkamvar@gmail.com>
2024-08-21 14:50:40 -0700

    fix thing

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -9,5 +9,5 @@ curl -L \
   -H "Authorization: Bearer ${GH_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${repo}/pages/deployments \
-  -d '{"artifact_id":"'"${artifact_url}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${ID_TOKEN}"'","preview":false}'
+  -d '{"artifact_url":"'"${artifact_url}"'","environment":"github-pages","pages_build_version":"'"${deploy_id}"'","oidc_token":"'"${ID_TOKEN}"'","preview":false}'
 
